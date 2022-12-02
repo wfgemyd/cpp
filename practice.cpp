ae437ab0d546688027a14e219158c5dc8c105195
daniel shutov <66516254+wfgemyd@users.noreply.github.com>
2022-12-02 21:02:42 +0100

    started to practice cpp again from scratch

---
+++ practice.cpp
@@ -0,0 +1,28 @@
+//Finding the max and min
+#include <vector>
+using namespace std;
+
+int min(vector<int> list){
+    int mini = list[0];
+	  int len = size(list);
+	  for (int i = 0; i <= len - 1; i++) {
+		  if (list[i] < mini) {
+			  mini = list[i];
+			  cout << mini;
+		}
+	}
+	return mini;
+}
+
+int max(vector<int> list){
+    int max = list[0];
+	  int len = size(list);
+	  for (int i = 0; i <= len - 1; i++) {
+		  if (list[i] > max) {
+			  max = list[i];
+			  cout << max;
+		}
+	}
+	return max;
+}
+//_____________________________________________________
