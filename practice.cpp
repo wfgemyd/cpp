28d2763df5f9dd52282c5922edcdbc22893a21e1
daniel shutov <66516254+wfgemyd@users.noreply.github.com>
2022-12-02 21:04:11 +0100

    Update practice.cpp

--- practice.cpp
+++ practice.cpp
@@ -3,24 +3,24 @@
 using namespace std;
 
 int min(vector<int> list){
-    int mini = list[0];
-	  int len = size(list);
-	  for (int i = 0; i <= len - 1; i++) {
-		  if (list[i] < mini) {
-			  mini = list[i];
-			  cout << mini;
+	int mini = list[0];
+	int len = size(list);
+	for (int i = 0; i <= len - 1; i++) {
+		if (list[i] < mini) {
+			mini = list[i];
+			cout << mini;
 		}
 	}
 	return mini;
 }
 
 int max(vector<int> list){
-    int max = list[0];
-	  int len = size(list);
-	  for (int i = 0; i <= len - 1; i++) {
-		  if (list[i] > max) {
-			  max = list[i];
-			  cout << max;
+	int max = list[0];
+	int len = size(list);
+	for (int i = 0; i <= len - 1; i++) {
+		if (list[i] > max) {
+			max = list[i];
+			cout << max;
 		}
 	}
 	return max;
