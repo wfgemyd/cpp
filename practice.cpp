2b4165f1a1d3de59f43e2660443151e184b2d219
daniel shutov <66516254+wfgemyd@users.noreply.github.com>
2022-12-03 15:33:37 +0100

    update
    made a guessing game

--- practice.cpp
+++ practice.cpp
@@ -26,3 +26,32 @@ int max(vector<int> list){
 	return max;
 }
 //_____________________________________________________
+#include <ctime>
+using namespace std;
+
+int main()
+{
+	int num, guess, tries;
+	tries = 0;
+	srand(time(NULL));
+	num = rand() % 100 + 1;
+	
+	cout << "**** GUESSING GAME ****";
+	cout << "Guess a number between 1-100.\n Good luck!\n";
+	do {
+		cout << "Give your best guess: ";
+		cin >> guess;
+		if (guess < num) {
+			tries++;
+			cout << "Cold\n";
+		}
+		else{
+			tries++;
+			cout << "Hot\n";
+		}
+	} while (guess != num);
+	cout << "it took you " << tries << " tries to guess: " << num;
+
+	return 0;
+}
+//_____________________________________________________
