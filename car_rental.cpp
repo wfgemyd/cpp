ef4ab16b4348fd262fe9d4edc0e60b866165902b
daniel shutov <66516254+wfgemyd@users.noreply.github.com>
2022-12-14 14:27:06 +0100

    tried new things and concepts
    I don't know how to use the vector in a way that it will not be global, or how to check if there is an object that is already existing with the same car number.

--- car_rental.cpp
+++ car_rental.cpp
@@ -1,21 +1,40 @@
 #include <iostream>
 #include <string>
-
+#include <vector>
+#include <tuple>
 using namespace std;
 
 class cParts {
-	string sEngine;
-	bool bTouchscreen;
-	int nWheelSize;
-	int nAirBags;
+public:
+	string *sEngine;
+	bool *bTouchscreen;
+	int *nWheelSize;
+	int *nAirBags;
 
 	cParts(string sEngine, bool bTouchscreen, int nWheelSize, int nAirBags) {
-		this->sEngine = sEngine;
-		this->bTouchscreen = bTouchscreen;
-		this->nWheelSize = nWheelSize;
-		this->nAirBags = nAirBags;
+		this->sEngine = new string;
+		this->bTouchscreen = new bool;
+		this->nWheelSize = new int;
+		this->nAirBags = new int;
+
+		*this->sEngine = sEngine;
+		*this->bTouchscreen = bTouchscreen;
+		*this->nWheelSize = nWheelSize;
+		*this->nAirBags = nAirBags;
 	}
-	
+	cParts(const cParts &p) {
+		sEngine = new string(*p.sEngine);
+		bTouchscreen = new bool(*p.bTouchscreen);
+		nWheelSize = new int(*p.nWheelSize);
+		nAirBags = new int(*p.nAirBags);
+	}
+	~cParts()
+	{
+		delete sEngine;
+		delete bTouchscreen;
+		delete nWheelSize;
+		delete nAirBags;
+	}
 };
 
 
@@ -27,9 +46,27 @@ private:
 	int year;
 	string sLeaseDate;
 	string sReturnDate;
+	vector <string> sPrevNumbers;
+	int BNumberValidation(int nRandNum) {
+		if (sPrevNumbers.size() == 10) {
+			cout << "The capasity has been reached\n";
+		}
+		for (int i=0; i==sPrevNumbers.size()-1; i++) {
+			if (sPrevNumbers[i]== to_string(nRandNum))
+			{
+				return false;
+			}
+			return true;
+		}
+	}
 
 	int nCarNumGen() {
-		return rand() % 1000;
+		int temp = rand() % 10;
+		if (BNumberValidation(temp)) {
+			sPrevNumbers.push_back(to_string(temp));
+			return temp;
+		}
+		nCarNumGen();
 	}
 public:
 	cCar(string sModel, string sLeaseDate,string sReturnDate, bool bAvailable, int year){
@@ -51,15 +88,32 @@ public:
 		return nCarNumber;
 	}
 
+	string vPrintDates() {
+		return "start:"+sLeaseDate + " end:" + sReturnDate;
+	}
+	void vShowAllInfo() {
+		cout << sModel << "\n" << aGetCarAvailabilty() << "\n" << aGetCarNumber() << "\n" << vPrintDates() << "\n" << year << "\n";
+		cout << "*******************************************\n*******************************************";
+	}
 
 
 };
 
 int main() {
-	cCar car1("Lada", "11/12/22", "19/12/23", true, 1999);
+	cCar car1("Lada", "1/12/22", "19/12/23", true, 1996);
+	cCar car2("aff", "3/12/22", "12/12/23", false, 1994);
+	cCar car3("Lhha", "7/12/22", "16/12/23", true, 1992);
+	cCar car4("LATJa", "14/12/22", "18/12/23", false, 1999);
+	cCar car5("AFG", "13/12/22", "15/12/23", true, 1943);
+	cCar car6("khyed", "11/12/22", "16/12/23", false, 2009);
+	
 
-	cout << car1.aGetCarModel() << "\n ";
-	cout << car1.aGetCarNumber() << "\n";
-	cout<< car1.aGetCarAvailabilty();
+	car1.vShowAllInfo();
+	car2.vShowAllInfo();
+	car3.vShowAllInfo();
+	car4.vShowAllInfo();
+	car5.vShowAllInfo();
+	car6.vShowAllInfo();
+
 	return 0;
 }
