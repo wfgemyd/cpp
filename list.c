c15c94a24c3b1435466e6fe46a12038ff62a01f5
daniel shutov <66516254+wfgemyd@users.noreply.github.com>
2023-01-09 21:18:29 +0100

    Update list.c
    mid insertion was added

--- list.c
+++ list.c
@@ -16,6 +16,7 @@ void print_list(Node* head) {
 		i++;
 		current = current->next; //tarversing the list
 	}
+	printf("___________________\n");
 }
 bool is_empty(Node* head) {
 	if (head == NULL) return true;
@@ -32,17 +33,64 @@ Node* insert_at_head(Node* head, int new_value) {
 	}
 }
 
+Node* insert_at_tail(Node* head, int new_value) {
+	Node* new_node = calloc(1, sizeof(Node)); //allocation of memory
+	new_node->value = new_value;
+	if (is_empty(&head)) return new_node;
+	else {
+		Node* current = head;
+		while (current->next != NULL) current = current->next;
+		current->next = new_node;
+		return head;
+	}
+}
+
+Node* insert_at_middle(Node* head, int new_value, int location) {
+	Node* new_node = calloc(1, sizeof(Node)); //allocation of memory
+	new_node->value = new_value;
+	if (is_empty(&head)) return new_node;
+	else {
+		Node* current = head;
+
+		for (int i = 0; i != location - 1; i++) {
+			current = current->next;
+		}
+		Node* temp = current->next;
+		new_node->next = temp;
+		current->next = new_node;
+		
+		return head;
+	}
+}
+
+void freeList(Node* head)
+{
+	Node* tmp;
+
+	while (head != NULL)
+	{
+		tmp = head;
+		head = head->next;
+		free(tmp);
+	}
+
+}
+
 int main() {
 
 	Node* list1_head = NULL;
 
 	list1_head = insert_at_head(list1_head, 7);
 	list1_head = insert_at_head(list1_head, 3);
-	list1_head = insert_at_head(list1_head, 5);
-	list1_head = insert_at_head(list1_head, 9);
+
+	list1_head = insert_at_tail(list1_head, 14);
+	list1_head = insert_at_tail(list1_head, 44);
 	print_list(list1_head);
 
+	insert_at_middle(list1_head, 66, 2);
+	print_list(list1_head);
 
+	freeList(list1_head);
 	/*
 	Node a, b, c;
 	a.value = 5;
