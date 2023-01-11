0cd6b32d57db284967fdfa963eb0826f8b09ef1b
daniel shutov <66516254+wfgemyd@users.noreply.github.com>
2023-01-11 19:09:53 +0100

    Update list.c
    delete and match func was added

--- list.c
+++ list.c
@@ -62,6 +62,7 @@ Node* insert_at_middle(Node* head, int new_value, int location) {
 		return head;
 	}
 }
+
 Node* delete_at_head(Node* head)
 {
 	if (is_empty(&head)) return NULL;
@@ -96,6 +97,60 @@ Node* delete_at_tail(Node* head)
 	}
 }
 
+Node* delete_first_match(Node* head, int delete_value, bool* was_deleted)
+{
+	if (head == NULL)
+	{
+		*was_deleted = false;
+		return NULL;
+	}
+
+	if (head->value == delete_value)
+	{
+		Node* temp = head->next;
+		free(head);
+		*was_deleted = true;
+		return temp;
+	}
+
+	Node* current = head->next;
+	Node* prev = head;
+
+	while (current != NULL)
+	{
+
+		if (current->value == delete_value)
+		{
+			prev->next = current->next;
+			free(current);
+			*was_deleted = true;
+			return head;
+		}
+
+		prev = current;
+		current = current->next;
+	}
+
+	*was_deleted = false;
+	return head;
+}
+
+Node* delete_all_matches(Node* head, int delete_value,
+	int* num_deleted)
+{
+	Node* current = head;
+	bool deleted = false;
+	*num_deleted = 0;
+
+	do
+	{
+		current = delete_first_match(current, delete_value, &deleted);
+		if (deleted) *num_deleted = *num_deleted + 1;
+	} while (deleted);
+
+	return current;
+}
+
 int length(Node* head) {
 	if (head == NULL) {
 		return 0;
@@ -110,6 +165,25 @@ bool is_member(Node* head, int find_val) {
 	else if (head->value == find_val)return true;
 	else return is_member(head->next, find_val);
 }
+
+int count_matches(Node* head, int find_val) {
+	if (is_empty)return 0;
+	else if (head->value == find_val) return 1 + count_matches(head->next, find_val);
+	else return count_matches(head->next, find_val);
+}
+
+void replace_matches(Node* head, int find_value, int replace_value)
+{
+	if (head != NULL)
+	{
+		if (head->value == find_value)
+			head->value = replace_value;
+
+		replace_matches(head->next, find_value,
+			replace_value);
+	}
+}
+
 
 void freeList(Node* head)
 {
@@ -135,14 +209,24 @@ int main() {
 	list1_head = insert_at_tail(list1_head, 44);
 	print_list(list1_head);
 
+	bool deleted =false;
+	
 	list1_head = insert_at_middle(list1_head, 66, 2);
 	list1_head = delete_at_head(list1_head);
 	
 	print_list(list1_head);
 	printf("The length of the list is: %d \n",length(list1_head));
 
+	replace_matches(list1_head, 7, 33);
+
 	if (is_member(list1_head, 7)) printf("it is in the list");
 	else printf("not here");
+
+	printf("\n \n________ \n \n");
+	list1_head = delete_first_match(list1_head, 44, &deleted);
+	
+	print_list(list1_head);
+
 	freeList(list1_head);
 	/*
 	Node a, b, c;
